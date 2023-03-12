c10597e1af273c70b385e5bbc6837da89c485f03
Nikita Bugakov <77589921+borbos2001@users.noreply.github.com>
2023-03-12 16:02:42 +0300

    TakeLoot

--- TestGame/Assets/Scripts/Bag.cs
+++ TestGame/Assets/Scripts/Bag.cs
@@ -1,24 +1,34 @@
-
 using System.Collections.Generic;
 using UnityEngine;
 public class Bag : MonoBehaviour
 {
-    [SerializeField] private Transform _bag;
-    private List<GameObject> _coobWheat = new List<GameObject>();
+    [SerializeField] private Transform _player;
+    [SerializeField] private GameObject _cubeWheat;
+    private List<GameObject> _cubeWheatList = new List<GameObject>();
+
+    private GameObject _bag;
+
+    private void Start()
+    {
+        _bag = GameObject.Find("Bag");
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("wow");
         if(other.tag == "Item")
         {
-            other.transform.SetParent(_bag);
-            IntoBag(other.gameObject);
+            other.tag = "Untagged";
+            other.transform.SetParent(_player);
+            GameObject wheat = other.gameObject;
+            wheat.GetComponent<ObjectOffset>().StartMove(true, _player, wheat.transform);
+            IntoBag();
         }
     }
-    public void IntoBag(GameObject wheat)
+    public void IntoBag()
     {
-        _coobWheat.Add(wheat);
-        _coobWheat.LastIndexOf(wheat);
-        wheat.GetComponent<TakeLoot>().StartMove(true,_bag,wheat.transform);
+        GameObject wheat = Instantiate(_cubeWheat, _bag.transform.position, _bag.transform.rotation,_bag.transform);
+        _cubeWheatList.Add(wheat);
+        int ind = _cubeWheatList.LastIndexOf(wheat);
+        wheat.transform.position += Vector3.up * _cubeWheat.transform.localScale.y/10 * ind ;
     }
     public void OutBag()
     {
--- TestGame/Assets/Scripts/ObjectOffset.cs
+++ TestGame/Assets/Scripts/ObjectOffset.cs
@@ -1,7 +1,7 @@
 
 using UnityEngine;
 
-public class TakeLoot : MonoBehaviour
+public class ObjectOffset : MonoBehaviour
 {
     private Transform _startPoint;
     private Transform _endPoint;
@@ -10,21 +10,29 @@ public class TakeLoot : MonoBehaviour
     [SerializeField] private float speed = 5f;
     private float distance;
     private float startTime;
-
     private void Update()
     {
         if (_take)
         {
+            distance = Vector3.Distance(gameObject.transform.position, _endPoint.position);
             MoveItem();
         }
     }
     private void MoveItem()
     {
-        float timeElapsed = (Time.time - startTime) * speed;
-        float normalizedDistance = timeElapsed / distance;
-        Vector3 pointA = Vector3.Lerp(_startPoint.position, _endPoint.position, normalizedDistance);
-        Vector3 pointB = pointA + Vector3.up * height;
-        gameObject.transform.position = ParabolicMovementEquation(pointA, pointB, _endPoint.position, normalizedDistance);
+        if (distance >= 0.1f)
+        {
+            float timeElapsed = (Time.time - startTime) * speed;
+            float normalizedDistance = timeElapsed / distance;
+            Vector3 pointA = Vector3.Lerp(_startPoint.position, _endPoint.position, normalizedDistance);
+            Vector3 pointB = pointA + Vector3.up * height;
+            gameObject.transform.position = ParabolicMovementEquation(pointA, pointB, _endPoint.position, normalizedDistance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
     }
     private Vector3 ParabolicMovementEquation(Vector3 pointA, Vector3 pointB, Vector3 pointC, float t)
     {
@@ -41,6 +49,7 @@ public class TakeLoot : MonoBehaviour
         _take = take;
         _endPoint = endPoint;
         _startPoint = startPoint;
+        startTime = Time.time;
     }
 
 }
